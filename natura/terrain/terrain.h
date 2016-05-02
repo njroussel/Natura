29e33c2648bad097f08f301f3e14785a6d7f2c78
JBouron <justinien.bouron@epfl.ch>
2016-05-02 11:31:46 +0200

    Terrain : Now uses a chunkFactory.

--- natura/terrain/terrain.h
+++ natura/terrain/terrain.h
@@ -4,15 +4,16 @@
 #include <deque>
 #include "../grid/grid.h"
 #include "chunk/chunk.h"
+#include "chunk/chunk_generation/chunk_factory.h"
 
 
 class Terrain {
 public:
-    Terrain(uint32_t chunk_per_side, uint32_t quad_side_size, PerlinNoise *perlinNoise) {
+    Terrain(uint32_t chunk_per_side, uint32_t quad_side_size, PerlinNoise *perlinNoise) : m_chunk_factory(quad_side_size, perlinNoise){
         for (int i = 0 ; i < chunk_per_side; i ++) {
-            std::deque<Chunk> row;
+            std::deque<Chunk *> row;
             for (int j = 0 ; j < chunk_per_side; j ++) {
-                row.push_back(Chunk(glm::vec2(i, j), quad_side_size, perlinNoise));
+                row.push_back(m_chunk_factory.createChunk(glm::vec2(i, j)));
             }
             m_chunks.push_back(row);
         }
@@ -23,7 +24,7 @@ public:
         m_skybox->Init();
         for (size_t i = 0 ; i < m_chunks.size() ; i ++) {
             for (size_t j = 0 ; j < m_chunks.size() ; j ++) {
-                m_chunks[i][j].Init();
+                m_chunks[i][j]->Init();
             }
         }
     }
@@ -34,7 +35,7 @@ public:
         m_skybox->Draw(projection * view * model);
         for (size_t i = 0 ; i < m_chunks.size() ; i ++) {
             for (size_t j = 0 ; j < m_chunks.size() ; j ++) {
-                m_chunks[i][j].Draw(amplitude, time, glm::translate(model, glm::vec3(i*CHUNK_SIDE_TILE_COUNT, 0.0, j*CHUNK_SIDE_TILE_COUNT)), view, projection);
+                m_chunks[i][j]->Draw(amplitude, time, glm::translate(model, glm::vec3(i*CHUNK_SIDE_TILE_COUNT, 0.0, j*CHUNK_SIDE_TILE_COUNT)), view, projection);
             }
         }
     }
@@ -42,12 +43,14 @@ public:
     void Cleanup(){
         for (size_t i = 0 ; i < m_chunks.size() ; i ++) {
             for (size_t j = 0 ; j < m_chunks.size() ; j ++) {
-                m_chunks[i][j].Cleanup();
+                m_chunks[i][j]->Cleanup();
+                delete m_chunks[i][j];
             }
         }
     }
 
 private:
-    std::deque<std::deque<Chunk>> m_chunks;
+    ChunkFactory m_chunk_factory;
+    std::deque<std::deque<Chunk *>> m_chunks;
     Cube* m_skybox;
 };
