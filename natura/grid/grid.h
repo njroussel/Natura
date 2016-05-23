cdca2e23e70ef752d1af59cbee0182676301c3b3
Nicolas Roussel <n.roussel13@gmail.com>
2016-05-23 10:40:56 +0200

    Remove useles code

--- natura/grid/grid.h
+++ natura/grid/grid.h
@@ -232,7 +232,7 @@ public:
             throw (string("Failed to load texture"));
         }
 
-        glGenTextures(1, &*texture_id);
+        glGenTextures(1, texture_id);
         glBindTexture(GL_TEXTURE_2D, *texture_id);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
