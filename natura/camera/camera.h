dbbc3c59d45899d41c1c4bf6210e45dea40c43a9
JBouron <justinien.bouron@epfl.ch>
2016-05-26 18:50:00 +0200

    Fix stuttering.

--- natura/camera/camera.h
+++ natura/camera/camera.h
@@ -63,8 +63,8 @@ public:
             //glm::vec3 look_point = -m_look_curve->getPosition(delta_time);
             glm::vec3 look_point = glm::vec3(0);
             glm::vec3 pos_point = -m_pos_curve->getPosition(delta_time);
-            lookAtPoint(glm::vec3(TERRAIN_SCALE * look_point.x, TERRAIN_SCALE * look_point.y, TERRAIN_SCALE * look_point.z));
             m_position = pos_point * TERRAIN_SCALE;
+            lookAtPoint(glm::vec3(TERRAIN_SCALE * look_point.x, TERRAIN_SCALE * look_point.y, TERRAIN_SCALE * look_point.z));
         }
     }
 
